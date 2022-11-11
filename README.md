a1d9c8bb457b7142a1a0f459e1c918646e22c98f
Olivier_Luethy <olivier@kauz.ch>
2022-11-11 10:47:42 +0100

    You don't have to enter amount of marks

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # WhichMarkDoINeed
-Ein Programm das ausrechnen, welche Note man braucht um einen bestimmten Schnitt zu erhalten.
+Ein Programm das ausrechnet, welche Note man braucht um den gewünschten Schnitt zu erreichen.
 
-Nehmen wir mal an, man hat einen schlechten Schnitt.
+## Hintergedanke
+Nehmen wir mal an, man hat einen schlechten Schnitt. Dann würden sich die Meisten folgendes überlegen: "Welche Note brauche ich, um meinen Schnitt ins Genügende zu bringen". Wie z.B. für eine 4. Ich selbst war schon ein paar Mal in der Situation und musste dann mehrmals mit dem Taschenrechner ausprobieren und schauen, welche Note mich wieder auf Vordermann bringt.
+
+Dazu nun die Idee, dass man alle Noten eingibt und am Ende angibt, welchen Schnitt man sich wünscht. Danach berechnet das Tool selbst, welche Note man braucht. Das macht das Leben sehr viel einfacher!
--- main.py
+++ main.py
@@ -1,31 +1,46 @@
 # The mark the user needs to get to the average
-magicMark = 0;
-# The calculation of all the marks
-totalMarks = 0;
+magicMark = 0
 # Container to store mark for check befor used
-checkMark = 0;
+checkMark = 0
+
+def typeMark():
+    # The calculation of all the marks
+    totalMarks = 0
+    # To get amount of marks
+    i = 0
+    checkMark = 0
+    while True:
+        checkMark = input("Please enter mark (s for stop):\t\t")
+        # Falls Zahl oder Kommazahl
+        try:
+            checkMark = float(checkMark)
+            totalMarks += checkMark
+            i += 1
+        # Falls Buchstabe
+        except:
+            getMagicMark(i, totalMarks)
+        
+# Calculate mark for needed average
+def getMagicMark(i, totalMarks):
+    wishedAverage = float(input("Please enter wished average:\t\t"))
+
+    print("amount ", i, " total ", totalMarks)
+    #Formula
+    magicMark = (i + 1) * wishedAverage - totalMarks
+
+    if magicMark > 6:
+        print("The wished mark isn't possible")
+    else:
+        print(f"You need the mark of {magicMark:.2f} to get the average of {wishedAverage:.2f}\n")
+
+    wantContinue = input("Do you want to calculate new (Y for Yes, N for No)?\t\t")
+    if wantContinue == "Y":
+        typeMark()
+    elif wantContinue == "N":
+        exit()
+
 # Main
 print("Welcome To BiteX")
 print("A program to calculate the mark you need to get the average wanted\n")
-amount = input("Please enter the amount of marks you have:\t\t")
 
-amount = int(amount)
-
-i = 0
-while i != amount:
-    checkMark = float(input("Please enter mark:\t\t"))
-    if checkMark > 6:
-        print("Mark isn't valid.")
-    else:
-        totalMarks += checkMark
-        i += 1
-
-wishedAverage = float(input("Please enter wished average:\t\t"))
-
-#Formula
-magicMark = (amount + 1) * wishedAverage - totalMarks
-
-if magicMark > 6:
-    print("The wished mark isn't possible")
-else:
-    print(f"You need the mark of {magicMark:.2f} to get the average of ", wishedAverage)
+typeMark()
