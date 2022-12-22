c4d72e823776fcffa526d739e62289e6ae251aa4
Olivier_Luethy <olivier@kauz.ch>
2022-12-22 08:27:03 +0100

    Code refactoring made by chat gpt

--- main.py
+++ main.py
@@ -1,57 +1,81 @@
-# The mark the user needs to get to the average
-magicMark = 0
-# Container to store mark for check befor used
-checkMark = 0
-
-def typeMark():
-    # The calculation of all the marks
-    totalMarks = 0
-    # To get amount of marks
-    i = 0
-    checkMark = 0
-    while True:
-        checkMark = input("Please enter mark (s for stop):\t\t")
-        # Falls Zahl oder Kommazahl
-        try:
-            checkMark = float(checkMark)
-            if checkMark > 6:
-                print("Mark isn't valid.")
-            else:
-                totalMarks += checkMark
-                i += 1
-        # Falls Buchstabe
-        except:
-            getMagicMark(i, totalMarks)
+def calculate_magic_mark(total_marks, num_marks, desired_average):
+    """
+    Calculates the mark a user needs to get to achieve a desired average.
+    
+    Parameters:
+        total_marks (float): The sum of all marks entered by the user.
+        num_marks (int): The number of marks entered by the user.
+        desired_average (float): The average the user wants to achieve.
         
-# Calculate mark for needed average
-def getMagicMark(i, totalMarks):
+    Returns:
+        float: The mark the user needs to get to achieve the desired average.
+    """
+    # Calculate the magic mark using the formula (num_marks + 1) * desired_average - total_marks
+    magic_mark = (num_marks + 1) * desired_average - total_marks
+    
+    # If the magic mark is greater than 6, it is not allowed and a message is printed
+    if magic_mark > 6:
+        print("The wished mark isn't possible")
+        return None
+    else:
+        return magic_mark
+    
+
+def get_marks():
+    """
+    Prompts the user to enter marks until they stop the input.
+    
+    Returns:
+        tuple: A tuple containing the sum of all marks and the number of marks entered by the user.
+    """
+    total_marks = 0
+    num_marks = 0
+    
+    # Loop until the user inputs 's' to stop
     while True:
-        wishedAverage = input("Please enter wished average:\t\t")
-        # Falls Zahl oder Kommazahl
+        mark = input("Please enter mark (s for stop):\t\t")
+        if mark == 's':
+            break
+        
         try:
-            wishedAverage = float(wishedAverage)
-            if wishedAverage > 6:
+            mark = float(mark)
+            # If the mark is greater than 6, it is not allowed
+            if mark > 6:
                 print("Mark isn't valid.")
             else:
-                #Formula
-                magicMark = (i + 1) * wishedAverage - totalMarks
+                # Add the mark to the total sum and increment the number of marks
+                total_marks += mark
+                num_marks += 1
+        # If the input cannot be converted to a float, it is not a valid mark
+        except ValueError:
+            print("Invalid input. Please enter a valid mark or 's' to stop.")
+    
+    # Return the total sum of marks and the number of marks
+    return total_marks, num_marks
 
-                if magicMark > 6:
-                    print("The wished mark isn't possible")
+
+def main():
+    print("Welcome To BiteX")
+    print("A program to calculate the mark you need to get the average wanted\n")
+    
+    # Loop the program until the user wants to exit
+    while True:
+        # Get the total sum of marks and the number of marks from the user
+        total_marks, num_marks = get_marks()
+        
+        # Loop until the user inputs a valid average
+        while True:
+            desired_average = input("Please enter wished average:\t\t")
+            try:
+                desired_average = float(desired_average)
+                # If the average is greater than 6, it is not allowed
+                if desired_average > 6:
+                    print("Mark isn't valid.")
                 else:
-                    print(f"You need the mark of {magicMark:.2f} to get the average of {wishedAverage:.2f}\n")
-
-                wantContinue = input("Do you want to calculate new (Y for Yes, N for No)?\t\t")
-                if wantContinue == "Y":
-                    typeMark()
-                elif wantContinue == "N":
-                    quit()
-        # Falls Buchstabe
-        except:
-            print("")
-
-# Main
-print("Welcome To BiteX")
-print("A program to calculate the mark you need to get the average wanted\n")
-
-typeMark()
+                    break
+            except ValueError:
+                print("Invalid input. Please enter a valid average.")
+        
+        # Calculate the magic mark and print the result if it is possible
+        magic_mark = calculate_magic_mark(total_marks, num_marks, desired_average)
+        if magic_mark is not None:
